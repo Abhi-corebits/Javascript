79e59ab7004993565669371198216c31bb449d47
Abhinav Pingle <pingleabhinav@gmail.com>
2025-12-02 06:51:14 +0000

    object_literals

--- stack_heap.js
+++ stack_heap.js
@@ -12,16 +12,27 @@ let user = {
     "age":19
 }
 
-console.log(user.name , user.age);
+// console.log(user.name , user.age);
 user2 = user
-console.log(user2.name , user2.age);
+// console.log(user2.name , user2.age);
 
 user2.name = "Sidd"
 
-console.log(user.name , user.age);
-console.log(user2.name , user2.age);
+// console.log(user.name , user.age);
+// console.log(user2.name , user2.age);
 
-console.log(newname.slice(-5 , 2));
+// console.log(newname.slice(-5 , 2));
 
+// Object literals 
+const mysy = Symbol("symbol_one")
+const data = {
+    [mysy] : "hey",  // If we want to use symbol datatype in a object
+    name : "Abhi" ,
+    age : 19 ,
+    days : ["Sunday" , "Monday"]
+}
 
+console.log(data[mysy]);
+console.log(typeof data[mysy]);
+console.log(data["days"]);
 
