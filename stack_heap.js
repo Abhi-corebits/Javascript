921ee91956150946c3cc68cf760a26da253b9417
Abhinav Pingle <pingleabhinav@gmail.com>
2025-12-02 05:43:09 +0000

    stack and heap understanding

---
+++ stack_heap.js
@@ -0,0 +1,24 @@
+let name = "abhi"
+// console.log(name)
+let newname = name;
+// console.log(newname)
+
+newname = "abhinav" 
+// console.log(name)
+// console.log(newname)
+
+let user = {
+    "name":"abhi" ,
+    "age":19
+}
+
+console.log(user.name , user.age);
+user2 = user
+console.log(user2.name , user2.age);
+
+user2.name = "Sidd"
+
+console.log(user.name , user.age);
+console.log(user2.name , user2.age);
+
+
