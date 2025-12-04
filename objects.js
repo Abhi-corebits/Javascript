949aba7372b10709db7d1bec50f1e21f79c103f9
Abhinav Pingle <pingleabhinav@gmail.com>
2025-12-04 05:02:50 +0000

    for loops

--- objects.js
+++ objects.js
@@ -24,3 +24,35 @@ const {cs : c} = obj["dept"]
 console.log(c); //for dict/object deconstruct
 
 console.log({} = obj["dept_name"][0]); //for list
+
+
+// Advance type of object known as map
+const map = new Map()
+map.set("IT","Vedant")
+map.set("CS","Siddharth")
+console.log(map.size)
+
+// console.log(map.get("IT"))
+for (const [key , value] of map) {
+    console.log(key , " " , value);
+}
+
+
+// you cant iterate through normal obj via for of
+// There are 3 types of for loop in js
+// 1.for each ---> used to iterate through an array only. it doesnt allows pass , stop or continue
+// 2.for of ---> used to iterate through iterables such as array , maps , strings , sets
+// 3.for in ---> used to iterate through objects , arrays
+
+const depts = {
+    "IT":"Vedant",
+    "CS":"Siddharth"
+}
+// for (const element of depts) {
+//     console.log(element);
+// }
+for (const key in depts) {
+    // if (!Object.hasOwn(object, key)) continue;
+    console.log(key , " " , depts[key]);
+    // const element = object[key];
+}
